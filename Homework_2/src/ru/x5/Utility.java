33c94b563712d8582df5a420dc8ef13c3af38a9b
Semen Pavelko <se.pa.94@mail.ru>
2020-09-02 07:02:43 +0300

    Add Utility class

---
+++ Homework_2/src/ru/x5/Utility.java
@@ -0,0 +1,16 @@
+package ru.x5;
+
+public final class Utility {
+
+    public static boolean isNumber(String stringNumber) {
+        try {
+            Integer.parseInt(stringNumber);
+            return true;
+        } catch (NumberFormatException e) {
+            System.out.println(String.format("This is not a number: \"%s\".", stringNumber));
+        }
+
+        return false;
+
+    }
+}
