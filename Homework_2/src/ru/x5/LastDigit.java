a3f4201be15d42bd78f5c12b00180a03a0899f3f
Semen Pavelko <se.pa.94@mail.ru>
2020-09-02 07:00:55 +0300

    Add homework 2.1 - Last number

---
+++ Homework_2/src/ru/x5/LastDigit.java
@@ -0,0 +1,33 @@
+package ru.x5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import static ru.x5.Utility.*;
+
+
+/*
+    1. Ввести целое число в консоли и вывести на экран
+    последнюю цифру введенного числа.
+*/
+public class LastDigit {
+
+    public static void main(String[] args) {
+
+        try (var reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String number = reader.readLine();
+            if (isNumber(number)) {
+                System.out.println(number.charAt(number.length() - 1));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+
+}
