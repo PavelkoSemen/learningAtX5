f33f2ab3f582d18961f3a0f413891c3953fce605
Semen Pavelko <se.pa.94@mail.ru>
2020-09-04 21:18:48 +0300

    Add homework 3.2 - Veterinary clinic

---
+++ Homework_3/src/ru/x5/animal/Horse.java
@@ -0,0 +1,29 @@
+package ru.x5.animal;
+
+public class Horse extends Animal {
+
+    int maneLength;
+
+    public Horse(String food, String location, int maneLength) {
+        super(food, location);
+        this.maneLength = maneLength;
+    }
+    public Horse(String food, String location){
+        this(food, location, 40);
+    }
+
+    public Horse(){
+        this("grass", "Street", 40);
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("The horse was awakened by the noise");
+    }
+
+    @Override
+    public void eat() {
+        super.eat();
+    }
+
+}
---
+++ Homework_3/src/ru/x5/animal/Veterinarian.java
@@ -0,0 +1,13 @@
+package ru.x5.animal;
+
+public class Veterinarian {
+
+
+    void treatAnimal(Animal animal) {
+        System.out.println(animal.getClass().getSimpleName() +
+                " eat " + animal.getFood() +
+                " location " + animal.getLocation());
+    }
+
+
+}
