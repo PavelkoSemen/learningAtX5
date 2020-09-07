afdd0ce428aad6e6269e09d0530ca57de3487ed1
Semen Pavelko <se.pa.94@mail.ru>
2020-09-07 21:21:38 +0300

    Add homework 2.7 - Replacing map with switch

--- Homework_2/src/ru/x5/LandlinePhone.java
+++ Homework_2/src/ru/x5/LandlinePhone.java
@@ -3,19 +3,12 @@ package ru.x5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
-
 /*
     7. Написать программу, вычисляющую стоимость 10
     минутного междугороднего разговора в зависимости
     от кода города.
 */
 public class LandlinePhone {
-    public static Map<String, Double> tariff = Map.of(
-            "905", 4.15,
-            "194", 1.98,
-            "491", 2.69,
-            "800", 5.00);
 
     public static void main(String[] args) {
 
@@ -27,15 +20,28 @@ public class LandlinePhone {
                 System.out.println("Enter city code: 905(Moscow), 194(Rostov), 491(Krasnodar), 800(Kirov)");
                 code = reader.readLine();
 
-            } while (!tariff.containsKey(code));
+            } while (!(tariffCost(code) != -1));
 
-            System.out.println(tariff.get(code) * talkTime);
-
+            System.out.println(tariffCost(code) * talkTime);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
+    public static double tariffCost(String tariffName) {
+
+        switch (tariffName) {
+            case "905":
+                return 4.15;
+            case "194":
+                return 1.98;
+            case "491":
+                return 2.69;
+            case "800":
+                return 5.00;
+            default:
+                return -1;
+        }
+    }
 }
